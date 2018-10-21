2dae247a13c104043956a6f837945745331dd5ca
sazad44 <530@holbertonschool.com>
2018-10-21 21:33:47 +0000

    Updating printf c file with opid function

--- printf.c
+++ printf.c
@@ -1,18 +1,24 @@
-#include <stdarg.h>
-#include "holberton.h"
-
 int _printf(const char *format, ...)
 {
 	int i;
 	va_list v_ls;
 
+	buffer *buf;
+	va_start(v_ls, format);
 	while (format[i])
 	{
-		opid(buffer, format);
-		buffer.mem[buffer.index] = format[i];
+		buffer.index += opid(buf, format, v_ls);
+		buffer.str[buffer.index] = format[i];
 		buffer.index++;
 		i++;
 	}
 	return (buffer.index);
 }
 
+int opid(buffer *buffer, char *format, va_list v_ls)
+{
+	if (format[*buffer.index] == '%')
+	{
+		for (; format[*buffer.index] < 'a' || format[*buffer.index] > 'z'; *buffer.index++)
+		{
+			
