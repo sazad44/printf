6630a219302b7949fa690ded34caec013adc599a
Martin Smith <538@holbertonschool.com>
2018-10-21 21:14:26 +0000

    Modify Buffer Functions.
    Remove:
    * buf_init(): replace by buf_new().
    
    Added:
    * buf_new()
    
    Changed:
    * buf_custom() : similar functionality as buf_new() but take a size argument.
    
    Fixes:
    * fix header for buffer_init
    * use all pointer to buf fields through out functions correctly using ‘->’ where needed.

--- buffer_int.c
+++ buffer_int.c
@@ -1,26 +1,41 @@
-#define "holberton.h"
-#define <stdio.h>
+#include "holberton.h"
+#include <stdio.h>
 /**
- * buf_init - Function that initialize a buffer at a default size of 1024
- * @buf: pointer to a buffer type.
+ * buf_init - Function that create a buffer at a default size of 1024
+ *
+ * Return: a pointer to a pointer.
  */
-void buf_init(buffer *buf)
+buffer *buf_new()
 {
+	buffer *buf;
+	
+	buf = malloc(sizeof(buffer));
+	if (!buf)
+		return (NULL);
 	buf->index = 0;
 	buf->size = 1024;
 	buf->str = malloc(sizeof(char) * buf->size + 1);
-}
 
+	return (buf);
+}
 /**
- * buf_custom - Function that initialize a buffer at a custom
- * @buf: pointer to a buffer type.
+ * buf_custom - Function that create a buffer at a custom
  * @size_uint
+ *
+ * Return: a pointer to a buffer
  */
-void buf_custom(buffer *buf, size_t size_uint)
+buffer *buf_custom(size_t size_uint)
 {
+	buffer *buf;
+	
+	buf = malloc(sizeof(buffer));
+	if (!buf)
+		return (NULL);
 	buf->index = 0;
 	buf->size = size_uint;
 	buf->str = malloc(sizeof(char) * buf->size + 1);
+
+	return (buf);
 }
 /**
  * but_end - frees up the buffer.
@@ -29,8 +44,6 @@ void buf_end(buffer *buf)
 {
 	if (!buf)
 		return;
-	free(buf->index);
-	free(buf->size);
 	free(buf->str);
 	free(buf);
 }
