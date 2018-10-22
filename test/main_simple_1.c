eafd103cd5c544e3dcf356910f2a33115ed9e9d3
sazad44 <530@holbertonschool.com>
2018-10-22 02:17:02 +0000

    Updating test directory

--- test/main_simple_1.c
+++ test/main_simple_1.c
@@ -11,10 +11,10 @@ int main(void)
 {
 	_printf("Let's try to printf a simple sentence.\n");
 	printf("Let's try to printf a simple sentence.\n");
-	_printf("Let's try to printf a %c sentence.\n", '4');
-	printf("Let's try to printf a %c sentence.\n", '4');
-	_printf("Let's try to printf a %s sentence.\n", "simple");
-	printf("Let's try to printf a %s sentence.\n", "simple");
+	_printf("Let's try to printf a %c sentence %c.\n", '4', '8');
+	printf("Let's try to printf a %c sentence %c.\n", '4', '8');
+	_printf("Let's try to printf a %s sentence %s.\n", "simple", "complex");
+	printf("Let's try to printf a %s sentence %s.\n", "simple", "complex");
 	_printf("Let's try to printf a %% sentence.\n");
 	printf("Let's try to printf a %% sentence.\n");
 	return (0);
