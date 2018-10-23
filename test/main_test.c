d76a633e7dfb9fadf7a4c34fa76c38111c12d875
Martin Smith <538@holbertonschool.com>
2018-10-23 02:45:36 +0000

    adding test folder

---
+++ test/main_test.c
@@ -0,0 +1,28 @@
+#include <limits.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int len;
+	int len2;
+	unsigned int ui;
+
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	ui = (unsigned int)INT_MAX + 1024;
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
+	_printf("Negative:[%d]\n", -762534);
+	printf("Negative:[%d]\n", -762534);
+	_printf("1Unsigned:[%u]\n", ui);
+	printf("2Unsigned:[%u]\n", ui);
+
+	printf("end");
+	return (0);
+}
